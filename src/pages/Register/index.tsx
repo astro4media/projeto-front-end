53e4a6013daab00a67800aa8f91bff157aeaf6d6
FelipeMurakami <murakamikazuo23@gmail.com>
2022-09-01 12:10:07 -0300

    erro register form input

--- src/pages/Register/index.tsx
+++ src/pages/Register/index.tsx
@@ -1,6 +1,9 @@
 import { motion } from "framer-motion";
+import FormInput from "../../components/formInput";
 
 const Register = () => {
+
+
   return (
     <motion.div
       initial={{ opacity: 0 }}
@@ -8,7 +11,22 @@ const Register = () => {
       exit={{ opacity: 0 }}
       transition={{ duration: 0.6 }}
     >
-      <h1>Register</h1>
+      <div className="">
+        <figure>
+          <img src="" alt="logo" />
+        </figure>
+        <form action="">
+          <FormInput 
+          id='id'
+          type='text'
+          label='name'
+          register=  
+          errors={Error}
+          >
+
+          </FormInput>
+        </form>
+      </div>
     </motion.div>
   );
 };
