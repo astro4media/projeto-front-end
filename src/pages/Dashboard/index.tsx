358156f456fcdbbaa2f868a7c1decbcc394fdb93
David <davidkauandkad@outlook.com.br>
2022-09-05 16:33:24 -0300

    feat: created header

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,15 +1,18 @@
 import { motion } from "framer-motion";
+import Header from "../../components/Header";
+import Container from "./styles";
 
 const Dashboard = () => {
   return (
-    <motion.div
+    <Container as={motion.div}
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       exit={{ opacity: 0 }}
       transition={{ duration: 0.6 }}
     >
+      <Header />
       <h1>Dashboard</h1>
-    </motion.div>
+    </Container>
   );
 };
 
