97778d2afa64dd800e7954936f5c04568a2f8c02
eliseubrito <eliseubrito@protonmail.com>
2022-09-06 03:47:51 -0300

    feat: Modal Complete

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -1,40 +1,53 @@
 import { ReactNode, useState } from "react";
-// import { MdHighlightOff } from "react-icons/md";
-import DeleteIcon from "@mui/icons-material/Delete;
+import HighlightOffIcon from "@mui/icons-material/HighlightOff";
 import { Link } from "react-router-dom";
-import { Modal,Box } from "@mui/material";
-import ModalStyle from "./styles"
+import { Modal, Box } from "@mui/material";
+import ModalStyle from "./styles";
 
 interface IModalProps {
-  open: true|false;
-  children?:ReactNode;
+  open: true | false;
+  children?: ReactNode;
   navigate: any;
   title?: String;
 }
 
-
-
-const ModalComponent = ({open, navigate, title, children}:IModalProps) => {
-  
-  
-  return(<>
-      <Modal open={open}>
+const ModalComponent = ({ open, navigate, title, children }: IModalProps) => {
+  return (
+    <>
+      <Modal
+        open={open}
+        sx={{
+          backdropFilter: "blur(3px)",
+          backgroundColor: "#00001e24",
+          zIndex: 1,
+        }}
+      >
         <ModalStyle>
-        <Box className="boxModal">
-          <Box className="topModal">
-            <h2>{title}</h2>
-            <Link to={navigate}>
-              <button>X</button>
-              <DeleteIcon></DeleteIcon>
-            </Link>
+          <Box className="boxModal">
+            {title ? (
+              <Box className="topModalTitle">
+                <h2 className="titleModal">{title}</h2>
+                <Link to={navigate}>
+                  <button className="buttonModal">
+                    <HighlightOffIcon></HighlightOffIcon>
+                  </button>
+                </Link>
+              </Box>
+            ) : (
+              <Box className="topModal">
+                <Link to={navigate}>
+                  <button className="buttonModal">
+                    <HighlightOffIcon></HighlightOffIcon>
+                  </button>
+                </Link>
+              </Box>
+            )}
+            <Box className="contentModal">{children}</Box>
           </Box>
-          <Box className="contentModal">
-            {children}
-          </Box>
-        </Box>
         </ModalStyle>
       </Modal>
-  </>);
+    </>
+  );
 };
 
 export default ModalComponent;
--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,6 @@
 import { motion } from "framer-motion";
-import { useState } from "react";
-import ModalComponent from "../../components/Modal";
 
 const Dashboard = () => {
-  const [open,setOpen] = useState(false);
   return (
     <motion.div
       initial={{ opacity: 0 }}
@@ -12,16 +9,6 @@ const Dashboard = () => {
       transition={{ duration: 0.6 }}
     >
       <h1>Dashboard</h1>
-      <div>
-        <button onClick={() => setOpen(true)}>ATIVAR</button>
-      <button onClick={() => setOpen(true)}>ATIVAR2</button>
-      <ModalComponent navigate='..' open={open}>
-        <div>
-          <h1>OI</h1>
-        </div>
-      </ModalComponent>
-      </div>
-
     </motion.div>
   );
 };
